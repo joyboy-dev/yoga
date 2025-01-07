53d564425e85f60185e641cf9315cf1aaf213047
rio.coder <sarfaraztech07@gmail.com>
2025-01-07 22:22:44 +0530

    add file in  components folder

---
+++ components/com1.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Components = () => {
+  return (
+    <View>
+      <Text>Components</Text>
+    </View>
+  )
+}
+
+export default Components
+
+const styles = StyleSheet.create({})
